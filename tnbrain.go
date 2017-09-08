169012e4429d6f83bf1d09d8e7ffdd332d8819d9
Leo Lahti <leo@Betelgauze.my.domain>
2017-09-09 00:28:06 +0300

    end of the day

--- tnbrain.go
+++ tnbrain.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"encoding/hex"
+	"fmt"
 	"log"
+	"net/http"
+	"strings"
+	"sync/atomic"
 	"time"
 
+	"github.com/TileHalo/tnparse"
 	"github.com/tarm/serial"
 )
 
 type (
 	Relay struct {
 		id     string
-		links  string
+		path   []string
 		status string
 		last   time.Time
 	}
@@ -30,29 +34,12 @@ type (
 	}
 )
 
-func StrToBinary(in string) string {
-	out := make(string)
-	for _, c := range in {
-		out = fmt.Sprintf(":s:.8b", out, c)
-	}
-	return out
-}
+const port = "/dev/ttyAMA0"
 
-func StrToHex(in string) string {
-	src := []byte(in)
-	dst := make([]byte, hex.EncodedLen(len(src)))
-	hex.Encode(dst, src)
-	return string(dst)
-}
-
-func HexToStr(src []byte) (string, error) {
-	dst := make([]byte, hex.DecodedLen(len(src)))
-	_, err := hex.Decode(dst, src)
-	return string(dst), err
-}
+var id uint64 = 0
 
 // Here the channels are buffered, so that it is asynchronous
-func Serial(in, out chan string) {
+func Serial(in, out chan []byte) {
 	conn := &serial.Config{Name: "TTYAMA0", Baud: 115200}
 	port, err := serial.OpenPort(conn)
 	if err != nil {
@@ -60,7 +47,7 @@ func Serial(in, out chan string) {
 	}
 	defer port.Close()
 
-	var msg string
+	var msg []byte
 	var pkg []byte
 	for {
 		pkg = make([]byte, 27) // This is inefficient and reads extra
@@ -69,23 +56,50 @@ func Serial(in, out chan string) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		out <- StrToBinary(HexToStr(pkg))
+		_, nerr := port.Write(msg)
+		if nerr != nil {
+			log.Fatal(err)
+		}
+		out <- pkg
 	}
 
 }
-
-func WS(in, out chan string) {
-
+func ToHavu(in, out chan string) {
+	for {
+		msg := <-in
+		r := strings.NewReader(msg)
+		_, err := http.Post("http://scout.polygame.org/api/msg", "text/plain", r)
+		if err != nil {
+			log.Println(err)
+		}
+	}
 }
 
-func MainLoop(in, out chan string) error {
+func MainLoop(in, out chan []byte, win, wout chan string) error {
+	devs := make([]Relay, 1)
+	devs[1] = Relay{"TB1", []string{""}, "", time.Now()}
+	for {
+		for _, dev := range devs {
+			smac := tnparse.MACSuper{int(id), 1, 0, dev.path}
+			out <- smac.NewMac()
+			cont := true
+			for cont {
+
+			}
+			fmt.Println(dev.id)
+
+			atomic.AddUint64(&id, 1)
+		}
+	}
 }
 
 func main() {
-	sin := make(chan string, 50)
-	sout := make(chan string, 50)
+	sin := make(chan []byte, 50)
+	sout := make(chan []byte, 50)
 
 	win := make(chan string, 50)
 	wout := make(chan string, 50)
 	go Serial(sout, sin)
+	go ToHavu(wout, win)
+	MainLoop(sin, sout, win, wout)
 }
