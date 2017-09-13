7314d1c668542247ff8294e5975b15b0bafd495b
Leo Lahti <leo.lahti1@gmail.com>
2017-09-13 16:38:13 +0300

    Refactored into discarding data not meant for the kesla

--- tnbrain.go
+++ tnbrain.go
@@ -5,9 +5,10 @@ import (
 	"log"
 	"net/http"
 	//"strings"
+	"fmt"
+	"os"
 	"sync/atomic"
 	"time"
-	"fmt"
 
 	"github.com/TileHalo/tnparse"
 	"github.com/tarm/serial"
@@ -40,8 +41,11 @@ const ser = "/dev/ttyAMA0"
 var (
 	pack_time        = 60 // ms
 	id        uint64 = 0
+	logfile          = "tacnetlog.log"
+	posfile          = "pos.log"
+	posfd     *os.File
 	port      *serial.Port
-	logfile   = "tacnetlog.log"
+	devs      []Relay
 )
 
 func CreateMacs(dev Relay, pkg tnparse.TNH) (tnparse.MACSuper, tnparse.MACSub) {
@@ -51,6 +55,63 @@ func CreateMacs(dev Relay, pkg tnparse.TNH) (tnparse.MACSuper, tnparse.MACSub) {
 	return smac, submac
 }
 
+func WaitMac(in chan []byte) tnparse.MACSuper {
+	var msg []byte
+	tt := make(chan bool)
+	go Timeout(tt, 1)
+	for {
+		select {
+		case msg = <-in:
+			if msg[0]&0x80 == 0x80 {
+				mac := tnparse.MACSuper{}
+				mac = mac.FromTNH(msg).(tnparse.MACSuper)
+				if mac.Jump_num == len(mac.Addr) {
+					return mac
+				}
+			}
+		case <-tt:
+			return tnparse.MACSuper{Pack_num: 0}
+		}
+
+	}
+}
+
+func HandlePacket(smsg []byte, in chan []byte, dev Relay, wout chan string) {
+	mac := tnparse.MACSuper{}
+	mac = mac.FromTNH(smsg).(tnparse.MACSuper)
+	if mac.Jump_num != len(mac.Addr) {
+		mac = WaitMac(in)
+		if mac.Pack_num == 0 {
+			return
+		}
+	}
+	tt := make(chan bool)
+	for i := 0; i < mac.Pack_num; {
+		var msg []byte
+		go Timeout(tt, 5.0)
+		select {
+		case msg = <-in:
+			mc := tnparse.MACSub{}
+			mc = mc.FromTNH(msg).(tnparse.MACSub)
+			switch mc.Packet.(type) {
+			case tnparse.POSREPLY:
+				p := mc.Packet.(tnparse.POSREPLY)
+				switch p.Pack.(type) {
+				case tnparse.POS:
+					_p := p.Pack.(tnparse.POS)
+					_p = _p.CalculateHavu(dev.id)
+					wout <- _p.Havu
+					i = mc.Pack_ord
+				}
+			default:
+				i = mac.Pack_num
+			}
+		case <-tt:
+			log.Printf("TIMEOUT RECEIVING PACKETS %s\n", dev.id)
+			return
+		}
+	}
+}
 func Timeout(t chan bool, dur int) {
 	time.Sleep(time.Duration(dur) * time.Second)
 	t <- true
@@ -109,7 +170,7 @@ func ToHavu(in, out chan string) {
 }
 
 func MainLoop(in, out chan []byte, win, wout chan string) error {
-	devs := []Relay{}
+	devs = []Relay{}
 	devs = append(devs, Relay{"KB1", []string{"__:", "KB1"}, "", time.Now()})
 	devs = append(devs, Relay{"TB1", []string{"__:", "KB1", "TB1"}, "", time.Now()})
 	// devs = append(Relay{"TB2", []string{"__:", "KB1", "TB2"}, "", time.Now()})
@@ -122,39 +183,10 @@ func MainLoop(in, out chan []byte, win, wout chan string) error {
 			out <- submac.ToTNH()
 
 			var smsg []byte
-			// tout := float64(pack_time*(len(dev.path)-2))/1000.0 + 0.4
 			go Timeout(tt, 5)
 			select {
 			case smsg = <-in:
-
-				mac := tnparse.MACSuper{}
-				mac = mac.FromTNH(smsg).(tnparse.MACSuper)
-				tt = make(chan bool)
-				for i := 0; i < mac.Pack_num; {
-					var msg []byte
-					go Timeout(tt, 5.0)
-					select {
-					case msg = <-in:
-						mc := tnparse.MACSub{}
-						mc = mc.FromTNH(msg).(tnparse.MACSub)
-						switch mc.Packet.(type) {
-						case tnparse.POSREPLY:
-							p := mc.Packet.(tnparse.POSREPLY)
-							switch p.Pack.(type) {
-							case tnparse.POS:
-								_p := p.Pack.(tnparse.POS)
-								_p = _p.CalculateHavu(dev.id)
-								wout <- _p.Havu
-								i = mc.Pack_ord
-							}
-						default:
-							i = mac.Pack_num
-						}
-					case <-tt:
-						log.Printf("TIMEOUT RECEIVING PACKETS %s\n", dev.id)
-						i = mac.Pack_num
-					}
-				}
+				HandlePacket(smsg, in, dev, wout)
 				atomic.AddUint64(&id, 1)
 			case <-tt:
 				log.Printf("TIMEOUT CALLING %s\n", dev.id)
@@ -168,10 +200,16 @@ func main() {
 	// Enable this to log into a file
 	// f, err := os.OpenFile(logfile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
 	// if err != nil {
-	// 	fmt.Printf("error opening file: %v", err)
+	// 	fmt.Printf("error opening file %s: %v",logfile,  err)
 	// }
 	// log.SetOutput(f)
 	// defer f.Close()
+	_fd, _err := os.Open(posfile)
+	if _err != nil {
+		fmt.Printf("Error opening file %s: %v", posfile, _err)
+	}
+	posfd = _fd
+	defer posfd.Close() // Might report an error
 
 	conn := &serial.Config{Name: ser, Baud: 115200}
 	_port, err := serial.OpenPort(conn)
@@ -185,9 +223,9 @@ func main() {
 
 	sin := make(chan []byte)
 	sout := make(chan []byte)
-
-	win := make(chan string, 50)
-	wout := make(chan string, 50)
+	// Buffered (hence, async) because internet might have a bad day.
+	win := make(chan string, 200)
+	wout := make(chan string, 200)
 	go SerialRead(sin)
 	go SerialWrite(sout)
 	go ToHavu(wout, win)
